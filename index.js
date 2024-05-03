fc787c526cfe496985724d621eb60c66a90c4244
Akshay <akshaysangani44@gmail.com>
2024-05-03 15:49:16 +0530

    ..

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cookieParser());
-const POST  =  process.env.PORT || 3000;
+const PORT  =  process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log('Server is running on port 3000!');
 });
